057fcca227b802b21b259c0d0d38706b73fb8a20
tidzi17 <tidzi15110@gmail.com>
2024-05-25 21:19:28 +0200

    cc

--- course101/functions/modularity.php
+++ course101/functions/modularity.php
@@ -1,1 +1,9 @@
-modularity
+<!-- Including Files in PHP
+include "heade.php"- Pull the file in here
+
+include_once "header.php" - Pull the file in here unless
+it has already been pulled in before
+
+require "header.php" - Pull in the file here and die if it is missing
+
+require_once "header.php" --->
